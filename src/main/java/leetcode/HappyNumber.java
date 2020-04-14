4744a3ceb1070bc5e028b1f5bc26b556cfc8ecd5
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-15 04:55:21 +0530

    Happy Number

---
+++ src/main/java/leetcode/HappyNumber.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class HappyNumber {
+    public boolean isHappy(int n) {
+        boolean res = false;
+        Set<Integer> set = new HashSet<>();
+        if(n==1)
+            return true;
+
+        while(!(n==1 || set.contains(n))){
+            set.add(n);
+            n = getNewN(n);
+            if(n==1){
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    private int getNewN(int n){
+        int res=0;
+
+        while (n > 0){
+            res += Math.pow(n%10,2);
+            n = n/10;
+        }
+        return res;
+    }
+}
