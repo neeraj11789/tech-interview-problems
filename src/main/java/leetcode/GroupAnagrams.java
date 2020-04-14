939f6bbcaed538da19336eb6d5d4b609a10fd888
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-15 04:25:23 +0530

    Group Anagrams - code cleanup

--- src/main/java/leetcode/GroupAnagrams.java
+++ src/main/java/leetcode/GroupAnagrams.java
@@ -7,23 +7,23 @@ class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         Map<String,List<String>> map = new HashMap<>();
-        List<String> sublist = new ArrayList<>();
 
         for(String s: strs){
             // incoming string to charArr
             // Sort and compare - add to hashmap
             char[] arr = s.toCharArray();
-            char[] s_arr = arr.clone(); Arrays.sort(s_arr);
+            char[] sorted_arr = arr.clone(); Arrays.sort(sorted_arr);
+            String sortedKey = new String(sorted_arr);
 
             // Check if this Anagram exists in the Map
-            if(map.containsKey(String.valueOf(s_arr))){
-                List<String> l = map.get(String.valueOf(s_arr));
+            if(map.containsKey(sortedKey)){
+                List<String> l = map.get(sortedKey);
                 l.add(s);
-                map.put(String.valueOf(s_arr), l);
+                map.put(sortedKey, l);
             }else{
                 List<String> l = new ArrayList<>();
                 l.add(s);
-                map.put(String.valueOf(s_arr), l);
+                map.put(sortedKey, l);
             }
 
         }
