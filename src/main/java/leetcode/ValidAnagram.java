0605c0cf75e2378cf7a9bab75fa8e53914a56a92
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-15 01:17:15 +0530

    Anagram with Sorting

---
+++ src/main/java/leetcode/ValidAnagram.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.util.Arrays;
+
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    }
+}
