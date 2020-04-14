846f0626b826ca7fb4423932c6ec5a7b1f45dd5b
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-15 05:28:06 +0530

    Counting Elements

---
+++ src/main/java/leetcode/CountingElements.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountingElements {
+    public int countElements(int[] arr) {
+        int res = 0;
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i: arr){
+            if(map.containsKey(i)){
+                int curr = map.get(i) + 1;
+                map.put(i, curr);
+            }else{
+                map.put(i, 1);
+            }
+        }
+
+
+        for(Map.Entry<Integer,Integer> m: map.entrySet()){
+            if(map.containsKey(m.getKey() + 1))
+                res += m.getValue();
+        }
+
+
+        return res;
+    }
+}
