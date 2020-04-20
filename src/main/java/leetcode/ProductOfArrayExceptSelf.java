ac8b8dd2613f096c0b96b8a5e2f63b1142690668
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-20 20:22:30 +0530

    product of array except self

---
+++ src/main/java/leetcode/ProductOfArrayExceptSelf.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ * Strategy Of Solution is important
+ * https://leetcode.com/problems/product-of-array-except-self/submissions/
+ */
+public class ProductOfArrayExceptSelf {
+	public int[] productExceptSelf(int[] nums) {
+		int len = nums.length;
+		int[] res = new int[len];
+		int[] L = new int[len];
+		int[] R = new int[len];
+
+		L[0] = R[len-1] = 1;
+
+		// Populate Left SubArray Product
+		int product = 1;
+		for(int i=1; i<len; i++){
+			product = product * nums[i-1];
+			L[i] = product;
+			System.out.println("L" + i + ": " + L[i]);
+		}
+
+		// Populate Right SubArray Product
+		product = 1;
+		for(int i=len-2; i>=0; i--){
+			product = product * nums[i+1];
+			R[i] = product;
+			System.out.println("R" + i + ": " + R[i]);
+		}
+
+		// Calculate Full Product
+		for(int i=0; i<len; i++){
+			res[i] = L[i] * R[i];
+		}
+
+		return res;
+	}
+}
