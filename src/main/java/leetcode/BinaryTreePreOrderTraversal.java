c68e9676efc19cc9735ae5d134e869728c0e70b7
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-21 15:07:30 +0530

    Binary Tree Pre Order Traversal - Non Recursive

---
+++ src/main/java/leetcode/BinaryTreePreOrderTraversal.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ */
+public class BinaryTreePreOrderTraversal {
+	/**
+	 * Pre Order Traversal - Non Recursive
+	 * @param root
+	 * @return
+	 */
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+		Stack<TreeNode> stack = new Stack<>();
+
+		TreeNode current = root;
+
+		while(current!=null || !stack.empty()){
+			while(current!=null){
+				// process
+				list.add(current.val);
+
+				stack.push(current);
+				current = current.left;
+			}
+
+			// process
+			current = stack.pop();
+			current = current.right;
+		}
+
+		return list;
+	}
+
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+}
