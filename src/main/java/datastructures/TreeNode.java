3e42ba16d17fc3e6890a284172a5d1969f02185e
Neeraj Gupta <neeraj11789@gmail.com>
2020-04-29 03:42:38 +0530

    Kth Smallest in the BST

---
+++ src/main/java/datastructures/TreeNode.java
@@ -0,0 +1,14 @@
+package datastructures;
+
+/**
+ * Binary Tree Node
+ */
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+}
