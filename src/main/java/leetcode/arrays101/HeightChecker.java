cd5585dd6468906c371b279617a2fd476f3e9a1b
Neeraj Gupta <neeraj11789@gmail.com>
2020-05-03 04:02:39 +0530

    Height Checker

---
+++ src/main/java/leetcode/arrays101/HeightChecker.java
@@ -0,0 +1,35 @@
+package leetcode.arrays101;
+
+/**
+ * @NOTE: Redo to understand better
+ *
+ * @LEARNING : Read More about Counting Sort
+ *
+ * https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3228/
+ *
+ */
+public class HeightChecker {
+	public int heightChecker(int[] heights) {
+		int[] heightToFreq = new int[101];
+
+		for (int height : heights) {
+			heightToFreq[height]++;
+		}
+
+		int result = 0;
+		int curHeight = 0;
+
+		for (int i = 0; i < heights.length; i++) {
+			while (heightToFreq[curHeight] == 0) {
+				curHeight++;
+			}
+
+			if (curHeight != heights[i]) {
+				result++;
+			}
+			heightToFreq[curHeight]--;
+		}
+
+		return result;
+	}
+}
