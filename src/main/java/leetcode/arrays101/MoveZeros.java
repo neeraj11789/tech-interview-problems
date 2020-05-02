22a7dbe0472b9b8bfb8f3825e5d869e9ff00684f
Neeraj Gupta <neeraj11789@gmail.com>
2020-05-03 02:08:26 +0530

    Move Zeros using Read & Write Pointer

---
+++ src/main/java/leetcode/arrays101/MoveZeros.java
@@ -0,0 +1,31 @@
+package leetcode.arrays101;
+
+public class MoveZeros {
+
+	public void moveZeroes(int[] nums) {
+
+		int N = nums.length;
+
+		int readPtr = 0;
+		int writePtr = 0;
+
+
+		while (readPtr < N){
+
+			if(nums[readPtr] != 0){
+
+				if(readPtr>writePtr){
+					nums[writePtr] = nums[readPtr];
+				}
+				writePtr++;
+			}
+
+			readPtr++;
+		}
+
+
+		while (writePtr < N) nums[writePtr++] = 0;
+
+	}
+
+}
