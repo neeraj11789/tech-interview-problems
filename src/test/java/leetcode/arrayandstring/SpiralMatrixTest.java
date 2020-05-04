af496437e5b2557f2533ddc5733dbf1424978c6d
Neeraj Gupta <neeraj11789@gmail.com>
2020-05-04 21:15:36 +0530

    Spiral Matrix

---
+++ src/test/java/leetcode/arrayandstring/SpiralMatrixTest.java
@@ -0,0 +1,30 @@
+package leetcode.arrayandstring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpiralMatrixTest {
+
+	SpiralMatrix obj;
+
+    @BeforeEach
+    void setup() {
+    	obj = new SpiralMatrix();
+    }
+
+    @Test
+    void check() {
+	    int[][] mat =
+			    {
+					    { 1,  2,  3,  4, 5},
+					    {16, 17, 18, 19, 6},
+					    {15, 24, 25, 20, 7},
+					    {14, 23, 22, 21, 8},
+					    {13, 12, 11, 10, 9}
+			    };
+
+	    obj.spiralOrder(mat);
+    }
+}
