8bfbed0874e90bb4f2e3291d47fd5262f7b5e2ce
Neeraj Gupta <neeraj11789@gmail.com>
2020-05-05 01:06:46 +0530

    Longest Common Prefix

---
+++ src/main/java/leetcode/arrayandstring/LongestCommonPrefix.java
@@ -0,0 +1,52 @@
+package leetcode.arrayandstring;
+
+/**
+ *
+ * @NOTE: Try implementing with trie #redo #revise
+ *
+ * https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/
+ */
+public class LongestCommonPrefix {
+	public String longestCommonPrefix(String[] strs) {
+
+		StringBuilder sb = new StringBuilder();
+
+		// Edge Cases
+		if (strs.length == 0) return "";
+		if (strs.length == 1) return strs[0];
+
+		// Find the min length substring
+		int minLength = Integer.MAX_VALUE;
+		int minLenghtIndex = 0;
+		for (int i=0; i<strs.length; i++){
+			if (strs[i].length() < minLength){
+				minLenghtIndex = i;
+				minLength = strs[i].length();
+			}
+		}
+
+		System.out.println("MinLength: " + minLength);
+
+		for (int i=0; i<minLength; i++){
+			char ch = strs[minLenghtIndex].charAt(i);
+
+			// Compare this character with all the strings in the bucket
+			boolean chFoundInAll = true;
+			for (int j=0; j<strs.length; j++){
+				if(ch != strs[j].charAt(i)){
+					chFoundInAll = false;
+					break;
+				}
+			}
+
+			if (chFoundInAll){
+				sb.append(ch);
+			}else {
+				// Break this loop as well if character differs in any string
+				break;
+			}
+		}
+
+		return sb.toString();
+	}
+}
