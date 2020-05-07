e05c2082ba4979389848b2001dfc5d2354338e73
Neeraj Gupta <neeraj11789@gmail.com>
2020-05-07 16:33:38 +0530

    String Questions

---
+++ src/main/java/leetcode/strings/ImplementStrStr.java
@@ -0,0 +1,40 @@
+package leetcode.strings;
+
+/**
+ * https://leetcode.com/problems/implement-strstr/
+ */
+public class ImplementStrStr {
+	public int strStr(String haystack, String needle) {
+		int res = -1;
+
+		// Empty String is always a match
+		if(needle.length() == 0) return 0;
+
+		if (haystack.length() == 0) return -1;
+
+		// Same Length but different Strings
+		if (haystack.length() == needle.length()){
+			return haystack.equals(needle)? 0 : -1;
+		}
+
+		for (int i=0; i<haystack.length(); i++){
+			int hIndex = i;
+			int nIndex = 0;
+
+			// Optimization to reduce comparision
+			if(i+needle.length() > haystack.length()) return -1;
+
+			while (hIndex<haystack.length() && nIndex<needle.length() &&
+			       haystack.charAt(hIndex) == needle.charAt(nIndex)){
+				hIndex++;
+				nIndex++;
+			}
+
+			// Have compared all the Characters of Needle
+			if(nIndex == needle.length())
+				return i;
+		}
+
+		return res;
+	}
+}
---
+++ src/main/java/leetcode/strings/ShiftingLetters.java
@@ -0,0 +1,18 @@
+package leetcode.strings;
+
+/**
+ * #revise
+ *
+ * https://leetcode.com/problems/shifting-letters/
+ */
+public class ShiftingLetters {
+	public String shiftingLetters(String S, int[] shifts) {
+		char[] arr = S.toCharArray();
+		int shift = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			shift = (shift + shifts[i]) % 26;
+			arr[i] = (char)((arr[i] - 'a' + shift) % 26 + 'a');
+		}
+		return new String(arr);
+	}
+}
